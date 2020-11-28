49982f9f3958b272b9bb5f3b24bf485a02b20059
CHERTIK <chertiksos@bk.ru>
2020-11-28 23:36:26 +0300

    + topics -> elevation

---
+++ app/src/main/java/com/rosseti/fragments/VideoFragment.kt
@@ -0,0 +1,32 @@
+package com.rosseti.fragments
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.VideoView
+import com.rosseti.R
+import com.rosseti.base.BaseFragment
+
+class VideoFragment(private val url: String): BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_video, container, false)
+        bindView(view)
+
+        return view
+    }
+
+    private fun bindView(view: View) {
+        val vView = view.findViewById<VideoView>(R.id.videoView)
+        vView.setVideoPath(url)
+        vView.start()
+    }
+
+
+}
